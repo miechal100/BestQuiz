7a7270d165527d22dc7bfea2f683a7ecf4856478
Michal Nurek <mnurek113@gmail.com>
2018-04-21 12:18:17 +0200

    initial commit

---
+++ app/src/main/java/com/code/knab/best_quiz/LectureActivity.kt
@@ -0,0 +1,12 @@
+package com.code.knab.best_quiz
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LectureActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lecture)
+    }
+}
