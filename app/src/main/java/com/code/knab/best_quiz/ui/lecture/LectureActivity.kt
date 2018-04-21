f6931eb5e8fe4287c1fcc6b6f97a9c658f51f9a9
Michal Nurek <mnurek113@gmail.com>
2018-04-21 13:24:50 +0200

    Lecture login screen view

--- app/src/main/java/com/code/knab/best_quiz/ui/lecture/LectureActivity.kt
+++ app/src/main/java/com/code/knab/best_quiz/ui/lecture/LectureActivity.kt
@@ -3,11 +3,17 @@ package com.code.knab.best_quiz.ui.lecture
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.code.knab.best_quiz.R
+import com.code.knab.best_quiz.ui.lecture.mvp.LectureMVP
 
-class LectureActivity : AppCompatActivity() {
-
+class LectureActivity : AppCompatActivity(), LectureMVP.View {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lecture)
+
+
+    }
+
+    override fun lectureLoaded() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
